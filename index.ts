e37c348e0d93f2b6f229bc10fa516c5f005c426d
nc-minh <nguyencongminh.official@gmail.com>
2023-06-30 16:56:34 +0700

    🚀

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import app from './src/app';
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 app.listen(port, () => {
   /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
+  console.log(`🚀 Listening: http://localhost:${port}`);
   /* eslint-enable no-console */
 });
